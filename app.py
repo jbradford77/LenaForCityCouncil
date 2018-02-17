48d96960f51fd68ab2d5201f29f19f48f9fa4c75
jbradford77 <jenn.bradford@yahoo.com>
2018-02-17 11:39:49 -0500

    footer changed to include

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Blogpost
 from datetime import datetime
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+from flask import make_response
+import requests
 
 app = Flask(__name__)
+
 
 # Connect to Database and create database session
 engine = create_engine('sqlite:///blog.db')
