b497cddf88be071ac1e296b44d40f566610bf80a
jbradford77 <jenn.bradford@yahoo.com>
2018-02-21 18:59:52 -0500

    contact page added

--- app.py
+++ app.py
@@ -48,12 +48,13 @@ def howToVote():
     return render_template('howToVote.html')
 
 
-
-
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
