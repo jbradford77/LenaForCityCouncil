036999654adbd35b288cc3118d47c5a26827f6c4
Jennifer Bradford <jenn.bradford@yahoo.com>
2018-02-23 08:52:47 -0500

    port 80 again

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = False
     app.run(threaded=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
