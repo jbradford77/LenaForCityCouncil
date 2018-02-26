1ee382a59cef3f5b868f97e138eeeb9d90f8e71e
jbradford77 <jenn.bradford@yahoo.com>
2018-02-26 18:45:06 -0500

    donate page a election date banners added also port 80

--- app.py
+++ app.py
@@ -52,10 +52,13 @@ def howToVote():
 def contact():
     return render_template('contact.html')
 
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
+
 
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = False
-    app.run(threaded=True)
+    app.debug = True
     app.run(host='0.0.0.0', port=80)
